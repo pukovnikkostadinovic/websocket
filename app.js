a8fd6b51cd3b37a91856d646c2a25312cf0a7cb5
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-11-24 15:03:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket = require('socket.io');
+
+var app = express();
+
+var server = app.listen(4000, function(){
+  console.log('listening to requests on port 4000');
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('made socket connection', socket.id);
+
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data);
+  });
+});
