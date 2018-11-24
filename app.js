6e529d66c1a48bad11c7435c85a1e836596af457
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-11-24 18:44:09 +0100

    dodal console log

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ io.on('connection', function(socket){
   console.log('made socket connection', socket.id);
 
   socket.on('chat', function(data){
+    console.log(data);
     io.sockets.emit('chat', data);
   });
 });
